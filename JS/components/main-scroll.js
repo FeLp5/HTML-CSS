3bc8e5a5ddd3216aa2cb082edc9b28c09e2d01c7
FeLp5 <empreenda165@gmail.com>
2022-07-03 17:30:47 -0300

    Corrgindo bug do header

--- JS/components/main-scroll.js
+++ JS/components/main-scroll.js
@@ -1,7 +1,7 @@
-const skill_btn = window.document.querySelector(".main-action.-sk");
-const projeto_btn = window.document.querySelector(".main-action.-pj");
-const sobre_btn = window.document.querySelector(".main-action.-ab");
-const contato_btn = window.document.querySelector(".main-action.-ct");
+const skill_btn = window.document.querySelector(".-sk");
+const projeto_btn = window.document.querySelector(".-pj");
+const sobre_btn = window.document.querySelector(".-ab");
+const contato_btn = window.document.querySelector(".-ct");
 const skill_container = window.document.querySelector(".main-section");
 
 // console.log(element)
@@ -9,7 +9,7 @@ const skill_container = window.document.querySelector(".main-section");
 skill_btn.addEventListener('click', function()
 {
     // alert('to ouvino')
-    window.scroll(0, 320)
+    window.scroll(0, 420)
 
 
 })
@@ -18,7 +18,7 @@ skill_btn.addEventListener('click', function()
 projeto_btn.addEventListener('click', function()
 {
     // alert('to ouvino')
-    window.scroll(0, 1200)
+    window.scroll(0, 1150)
 
 
 })
@@ -26,7 +26,7 @@ projeto_btn.addEventListener('click', function()
 sobre_btn.addEventListener('click', function()
 {
     // alert('to ouvino')
-    window.scroll(0, 1710)
+    window.scroll(0, 1610)
 
 
 })
@@ -34,7 +34,7 @@ sobre_btn.addEventListener('click', function()
 contato_btn.addEventListener('click', function()
 {
     // alert('to ouvino')
-    window.scroll(0, 3500)
+    window.scroll(0, 2200)
 
 
 })
