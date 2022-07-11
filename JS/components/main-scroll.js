69717010921a46687470a34c14ade010e94f221d
FeLp5 <empreenda165@gmail.com>
2022-07-11 12:34:23 -0300

    ajustando coordenadas de scroll

--- JS/components/main-scroll.js
+++ JS/components/main-scroll.js
@@ -4,12 +4,22 @@ const sobre_btn = window.document.querySelector(".-ab");
 const contato_btn = window.document.querySelector(".-ct");
 const skill_container = window.document.querySelector(".main-section");
 
+const skill_btn_menu = window.document.querySelector(".-sk-menu");
+const projeto_btn_menu = window.document.querySelector(".-pj-menu");
+const sobre_btn_menu = window.document.querySelector(".-ab-menu");
+const contato_btn_menu = window.document.querySelector(".-ct-menu");
+
+
+
+
+const section_project = window.document.querySelector(".main-section")
+
 // console.log(element)
 
 skill_btn.addEventListener('click', function()
 {
     // alert('to ouvino')
-    window.scroll(0, 420)
+    window.scroll(0, 410)
 
 
 })
@@ -35,6 +45,41 @@ contato_btn.addEventListener('click', function()
 {
     // alert('to ouvino')
     window.scroll(0, 2200)
+
+
+})
+
+//cellphone
+
+skill_btn_menu.addEventListener('click', function()
+{
+    // alert('to ouvino')
+    window.scroll(0, 410)
+
+
+})
+
+
+projeto_btn_menu.addEventListener('click', function()
+{
+    // alert('to ouvino')
+    window.scroll(0, 1540)
+
+
+})
+
+sobre_btn_menu.addEventListener('click', function()
+{
+    // alert('to ouvino')
+    window.scroll(0, 1930)
+
+
+})
+
+contato_btn_menu.addEventListener('click', function()
+{
+    // alert('to ouvino')
+    window.scroll(0, 2500)
 
 
 })
