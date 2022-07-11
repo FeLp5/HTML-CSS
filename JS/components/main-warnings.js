3d0eea66daa22173ffdbb74382f3d5b9115ea05e
FeLp5 <empreenda165@gmail.com>
2022-07-11 12:36:18 -0300

    Adicionando avisos ao client

---
+++ JS/components/main-warnings.js
@@ -0,0 +1,18 @@
+const btn_pj1 = window.document.querySelector(".calculator")
+const btn_pj2 = window.document.querySelector(".to-do-list")
+
+
+btn_pj1.addEventListener('click', function()
+{
+    alert('Pagina em desenvolvimento!!')
+
+
+})
+
+
+
+btn_pj2.addEventListener('click', function()
+{
+    alert("Pagina em desenvolvimento!!")
+
+})
