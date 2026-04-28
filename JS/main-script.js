d41f18287e590a548cde16ea77d22c48adfc8124
FeLp5 <empreenda165@gmail.com>
2026-04-27 23:10:02 -0300

    usando animejs

--- JS/main-script.js
+++ JS/main-script.js
@@ -1,8 +1,19 @@
-const meu_nome = document.querySelector("main-header")
+window.addEventListener('DOMContentLoaded', () => {
+  const { createTimeline, stagger, splitText } = anime;
 
+  const { words, chars } = splitText('h1', {
+    words: { wrap: 'clip' },
+    chars: true,
+  });
 
-meu_nome.addEventListener('click',()=>{
-
-
-    
-})
+  createTimeline({
+    loop: true,
+    defaults: { ease: 'inOut(2)', duration: 650 }
+  })
+  .add(words, {
+    y: ['5', '0%'],
+  }, stagger(250))
+  .add(chars, {
+    y: '5%',
+  }, stagger(10, { from: 'random' }));
+});
